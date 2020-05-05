6fe3ae8ec6b48ab8336d1d05ee33f350944c055c
Ava Tan <avatan1109@gmail.com>
2020-05-05 00:27:46 -0400

    Create class Workspace

--- src/db.py
+++ src/db.py
@@ -1,3 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class Workspace(db.Model):
+    __tablename__ = "workspace"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String, nullable = False)
+    url = db.Column(db.String, nullable = False)
+    users = db.relationship('User', secondary = )
+    channels = db.relationship('Channel', cascade = "delete")
+    dm_groups = db.relationship('')
