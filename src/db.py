ea0c86e84a7a8ad8c8c0d2223e64cd57ac7f45db
Allison Hsu <allison.hsu20@gmail.com>
2020-05-10 17:57:51 -0400

    added more to db

--- src/db.py
+++ src/db.py
@@ -1,12 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
-
+from time import time, ctime
 db = SQLAlchemy()
 
 association_table_userworksp = db.Table('association_userworksp', db.Model.metadata,
     db.Column('workspace_id', db.Integer, db.ForeignKey('workspace.id')),
     db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
     )
-
 
 
 
@@ -28,9 +27,16 @@ class Workspace(db.Model):
             'id':self.id,
             'name':self.name,
             'url':self.url,
-            'users':[a.serialize() for a in self.users],
-            'channels':[s.serialize() for s in self.channels],
-            'direct messages':[c.serialize() for c in self.dm_groups]
+            'users':[a.serialize_name() for a in self.users],
+            'channels':[s.serialize_name() for s in self.channels],
+            'direct messages':[c.serialize_for_channel() for c in self.dm_groups]
+        }
+
+    def serialize_name(self):
+        return{
+            'id':self.id,
+            'name':self.name,
+            'url':self.url
         }
 
            
@@ -48,6 +54,7 @@ class User(db.Model):
     messages = db.relationship('User', cascade='delete') #relationship one (user) to many (dms)
     dms = db.relationship('DM', cascade='delete') #relationship one (user) to many (dms)
     dm_messages = db.relationship('DMmessages', cascade='delete')
+    threads = db.relationship('Thread', cascade='delete')
 
     def __init__(self, **kwargs):
         self.name = kwargs.get('name', '')
@@ -66,11 +73,17 @@ class User(db.Model):
             'status':self.status,
             'active':self.active,
             'do_not_disturb':self.do_not_disturb,
-            'workspaces':[a.serialize() for a in self.workspaces],
-            'channels':[s.serialize() for s in self.channels],
-            'direct messages':[c.serialize() for c in self.dms]
+            'workspaces':[a.serialize_name() for a in self.workspaces],
+            'channels':[s.serialize_name() for s in self.channels],
+            'direct messages':[c.serialize_for_channel() for c in self.dms]
             #wont return dm messages and messages
         } 
+
+    def serialize_name(self):
+        return{
+            'id':self.id,
+            'name':self.name
+        }
 
 
 
@@ -82,22 +95,31 @@ class Channel(db.Model):
     workspace =  db.Column(db.Integer, db.ForeignKey('workspace.id'), nullable=False) #relationship one(workspace) to many (channel)
     users = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) #relationship one (channel) to many (users)
     messages = db.Column(db.Integer, db.ForeignKey('message.id'), nullable=False) #relationshipc one(channel) to many (users)
-    public = db.Column(db.String, nullable=True) #public or private
+    public = db.Column(db.Boolean, nullable=True) #public or private
     
     def __init__(self, **kwargs):
         self.name = kwargs.get('name', '')
         self.description = kwargs.get('description', '')
+        self.workspace = kwargs.get('workspace', '')
+        self.public = kwargs.get('public', '')
 
      def serialize(self):
         return{
             'id':self.id,
             'name':self.name,
-            'description':self.description,
+            'description':self.description,)
             'workspace':self.workspace.serialize(),
             'public': self.public,
-            'users':[a.serialize() for a in self.users],
-            'messages':[s.serialize() for s in self.messags],
+            'users':[a.serialize_name() for a in self.users],
+            'messages':[s.serialize(serialize_for_channel() for s in self.messags],
         } 
+
+     def serialize_name(self):
+         return{
+             'id':self.id,
+            'name':self.name,
+            'description':self.description
+         }
 
         
 class Message(db.Model):
@@ -112,37 +134,70 @@ class Message(db.Model):
     def __init__(self, **kwargs):
         self.sender = kwargs.get('sender', '')
         self.content = kwargs.get('content', '')
+        self.timestamp = ctime()
+        self.channel = kwargs.get('channel', '')
 
     def serialize(self):
         return{
             'id':self.id,
-            'sender':self.sender.serialize(),
+            'sender':self.sender.serialize_name(),
+            'content':self.content,
+            'timestamp':self.timestamp,
+            'channel':self.channel.serialize_name(),
+            'thread' = self.thread.serialize_for_message()
+        }    
+
+    def serialize_for_thread(self):
+        return{
+            'id':self.id,
+            'sender':self.sender.serialize_name(),
             'content':self.content,
             'timestamp':self.timestamp,
             'channel':self.channel.serialize(),
-            'thread' = self.thread.serialize()
-        }    
-        #add serialize for thread- it wont print the message then
-        #add serialze for channel- it wont have channel then
+        }
+
+    def serialize_for_channel(self):
+        return{
+            'id':self.id,
+            'sender':self.sender.serialize_name(),
+            'content':self.content,
+            'timestamp':self.timestamp,
+            'thread' = self.thread.serialize_for_message() 
+        }
+
+
 
 
 class Thread(db.Model): 
     __tablename__='thread'
     id = db.Column(db.Integer, primary_key = True)
     timestamp = db.Column (db.String, nullable = False)
+    sender = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     content = db.Column (db.String, nullable = False)
     message = db.Column(db.Integer, db.ForeignKey('message.id'), nullable=False) #relationship one to one
 
     def __init__(self, **kwargs):
+        self.sender = kwargs.get('sender', '')
         self.content = kwargs.get('content', '')
+        self.timestamp = ctime()
+        self.message = kwargs.get('message', '')
 
     def serialize(self):
         return{
             'id':self.id,
             'timestamp':self.timestamp,
+            'sender':self.sender.serialize_name(),
             'content':self.content,
-            'message':self.message.serialize()
+            'message':self.message.serialize_for_thread()
         }
+
+     def serialize_for_message(self):
+        return{
+            'id':self.id,
+            'timestamp':self.timestamp,
+            'sender':self.sender.serialize_name(),
+            'content':self.content
+        }    
 
            
 
@@ -150,23 +205,28 @@ class Thread(db.Model):
 class DM(db.Model):
     __tablename__='dm'
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String, nullable=True)
     workspace = db.Column(db.Integer, db.ForeignKey('workspace.id'), nullable=False)#relationship one (workspace) to many (dms)
     users = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     messages = db.relationship('DMmessage', cascade='delete')
 
     def __init__(self, **kwargs):
-        self.name = kwargs.get('name', '')
+        self.workspace = kwargs.get('workspace', '')
 
     def serialize(self):
         return{
             'id':self.id,
-            'name':self.name,
-            'woorkspace':self.workspace.serialize(), #just print name instead of serailzing? 
-            'users':[a.serialize() for a in self.users], #just print name?
-            'messages':[s.serialize() for s in self.messages]
+            'workspace':self.workspace.serialize_name(), 
+            'users':[a.serialize_name() for a in self.users], 
+            'messages':[s.serialize_for_dm() for s in self.messages]
         }
 
+    
+    def serialize_for_dmmessage(self):
+        return{
+            'id':self.id,
+            'workspace':self.workspace.serialize_name(), 
+            'users':[a.serialize_name() for a in self.users], 
+        }
            
 
 class DMmessage(db.Model):
@@ -179,19 +239,28 @@ class DMmessage(db.Model):
 
     def __init__(self, **kwargs):
         self.content = kwargs.get('content', '')
+        self.timestamp = ctime()
+        self.dm = kwargs.get('dm', '')
 
     def serialize(self):
+        return{
+            'id':self.id,
+            'sender':self.sender.serialize_name(),
+            'content':self.name,
+            'timestamp':self.timestamp,
+            'dm':self.dm.serialize_for_dmmessages()
+        }
+        
+    def serialize_for_dm(self):
         return{
             'id':self.id,
             'sender':self.sender.serialize(),
             'content':self.name,
             'timestamp':self.timestamp,
-            'dm':self.dm.serialize()
         }
            
 
 # many: db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
 # one: db.relationship('Assignments', cascade='delete')
 
-#how do i write a one to one relationship
-#how do i make certain fields optional when initing
+
