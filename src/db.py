204b3a8cd3b053240eb1b8a8346d17b477f650e3
Allison Hsu <allison.hsu20@gmail.com>
2020-05-09 23:51:25 -0400

    add assoc tables

--- src/db.py
+++ src/db.py
@@ -7,24 +7,47 @@ association_table_userworksp = db.Table('association_userworksp', db.Model.metad
     db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
     )
 
+association_table_chnworksp= db.Table('association_userworksp', db.Model.metadata,
+    db.Column('workspace_id', db.Integer, db.ForeignKey('workspace.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+
+association_table_chnusers= db.Table('association_userworksp', db.Model.metadata,
+    db.Column('workspace_id', db.Integer, db.ForeignKey('workspace.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+
+association_table_chnworksp= db.Table('association_userworksp', db.Model.metadata,
+    db.Column('workspace_id', db.Integer, db.ForeignKey('workspace.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+
+association_table_chnmsgs= db.Table('association_userworksp', db.Model.metadata,
+    db.Column('workspace_id', db.Integer, db.ForeignKey('workspace.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+
+association_table_chnthreads= db.Table('association_userworksp', db.Model.metadata,
+    db.Column('workspace_id', db.Integer, db.ForeignKey('workspace.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+
 class Workspace(db.Model):
     __tablename__ = "workspace"
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String, nullable = False)
     url = db.Column(db.String, nullable = False)
-    users = db.relationship("User", secondary = )
-    channels = db.relationship("Channel", cascade = "delete")
-    dm_groups = db.relationship('')
-
-    def __init__(self, **kwargs):
-        self.name = kwargs.get("name", "")
-        self.url = kwargs.get("url", "")
-
-    def serialize(self):
     users = db.relationship('User', secondary = association_table_userworksp, back_populates='workspaces')
     channels = db.relationship('Channel', cascade = "delete")
     dm_groups = db.relationship('')
 
+association_table_threaduser= db.Table('association_userworksp', db.Model.metadata,
+    db.Column('workspace_id', db.Integer, db.ForeignKey('workspace.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+
+association_table_threadmsg= db.Table('association_userworksp', db.Model.metadata,
+    db.Column('workspace_id', db.Integer, db.ForeignKey('workspace.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+    
+association_table_dmmsgs= db.Table('association_userworksp', db.Model.metadata,
+    db.Column('workspace_id', db.Integer, db.ForeignKey('workspace.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+    
 
 class User(db.Model):
     __tablename__='user'
@@ -36,8 +59,7 @@ class User(db.Model):
     active = db.Column (db.Boolean, nullable = False)
     do_not_disturb = db.Column (db.Boolean, nullable = False)
     workspaces = db.relationship('Workspace', secondary=association_table_userworksp, back_populates='users')
-<<<<<<< HEAD
-=======
+    
 
 
 class Channel(db.Model):
@@ -47,6 +69,7 @@ class Channel(db.Model):
     description = db.Column(db.String, nullable=True)
     workspace_id = #relationship
     users = #relationship
+    messages = #relationship
     public = db.Column(db.String, nullable=True) #public or private
     
 class Message(db.Model):
@@ -67,18 +90,14 @@ class Thread(db.Model):
 class DM(db.Model):
     __tablename__='dm'
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column (db.String, nullable = False)
-    name = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, nullable=False)
-    status = db.Column(db.String, nullable=True)
-    active = db.Column (db.Boolean, nullable = False)
-    do_not_disturb = db.Column (db.Boolean, nullable = False)
-    workspaces = db.relationship('Workspace', secondary=association_table_userworksp, back_populates='users')
-    
+    name = db.Column(db.String, nullable=True)
+    workspaces = #relationship
+    users = #relationship
+    messages =
+
 class DMmessage(db.Model):
     __tablename__='dm_message'
     id = db.Column(db.Integer, primary_key = True)
     sender = db.Column (db.String, nullable = False)
     timestamp = db.Column (db.String, nullable = False)
     dm = #relationship
->>>>>>> d2f6303d905a49a2f2d42e10f3127357f2a0d16b
